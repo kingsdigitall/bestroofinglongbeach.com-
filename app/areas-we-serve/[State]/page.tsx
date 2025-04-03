be06c1a5038155f5b873b1925e9b838c6f192c2d
Satnam-Gill <ersatnamgill@gmail.com>
2025-04-03 16:07:27 +0530

    (Update footer and contact information; enhance JSON-LD structured data for SEO)

--- app/areas-we-serve/[State]/page.tsx
+++ app/areas-we-serve/[State]/page.tsx
@@ -74,8 +74,79 @@ export default function SubdomainPage({ params }: SubdomainPageProps) {
     .filter((key) => key !== State)
     .map((key) => cityData[key]);
   // console.log(ContentData)
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    name: `${ContactInfo.name}`,
+    image:
+      `${ContactInfo.baseUrl}_next/image/?url=%2Flogo.png&w=1080&q=75` || "",
+    "@id": `${ContactInfo.baseUrl}`,
+    url: `${ContactInfo.baseUrl}`,
+    telephone: `${ContactInfo.No}`,
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: ContactInfo.address.split(",")[0].trim(),
+      addressLocality: ContactInfo.location.split(",")[0].trim(),
+      addressRegion: ContactInfo.location.split(",")[1].trim(),
+      postalCode: ContactInfo.zipCode.trim(),
+      addressCountry: "United States",
+    },
+    areaServed: {
+      "@type": "Place",
+      name: `${ContentData?.name}`,
+      address: {
+        "@type": "PostalAddress",
+        postalCode: `${ContentData?.zipCodes.split("|")[0]}`,
+        addressRegion: `${State.split("-").pop()?.toUpperCase()}`,
+        addressCountry: "United States",
+      },
+    },
+    priceRange: "$$",
+    openingHoursSpecification: [
+      {
+        "@type": "OpeningHoursSpecification",
+        dayOfWeek: [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+        ],
+        opens: "10:30",
+        closes: "12:32",
+      },
+    ],
+    aggregateRating: {
+      "@type": "AggregateRating",
+      ratingValue: 4,
+      reviewCount: 79,
+    },
+    potentialAction: {
+      "@type": "ReserveAction",
+      target: {
+        "@type": "EntryPoint",
+        urlTemplate: "#book_now",
+        inLanguage: "en-US",
+        actionPlatform: [
+          "http://schema.org/DesktopWebPlatform",
+          "http://schema.org/MobileWebPlatform",
+        ],
+      },
+      result: {
+        "@type": "Reservation",
+        name: `https://${ContactInfo.host}#Appointment`,
+      },
+    },
+  };
   return (
     <div className="mx-auto max-w-[2100px] overflow-hidden">
+      <section>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </section>
       <Banner
         h1={`${ContentData.h1Banner} ${ContentData.zipCodes && ContentData.zipCodes.split("|")[0]}`}
         image={ContentData.bannerImage}
@@ -86,7 +157,7 @@ export default function SubdomainPage({ params }: SubdomainPageProps) {
       {/* <p>{subDomain.map((item:any)=>(
         <p>{item}</p>
       ))}</p> */}
-      <div className="mt-14 grid w-full grid-cols-1 gap-6  px-6 md:mt-28 md:grid-cols-2 md:px-24 items-center">
+      <div className="mt-14 grid w-full grid-cols-1 items-center  gap-6 px-6 md:mt-28 md:grid-cols-2 md:px-24">
         <div className=" h-full">
           <Image
             height={1000}
@@ -96,37 +167,37 @@ export default function SubdomainPage({ params }: SubdomainPageProps) {
             alt={ContentData?.h2Image.split(".")[0]}
           />
         </div>
-          <div className=" flex w-full flex-col gap-3 ">
-            <span className="text-sm font-bold text-main">
-              {ContentData?.name} {ContactInfo.name}{" "}
-              Services
-            </span>
-            <h2 className="text-3xl font-bold">{ContentData?.h2}</h2>
+        <div className=" flex w-full flex-col gap-3 ">
+          <span className="text-sm font-bold text-main">
+            {ContentData?.name} {ContactInfo.name} Services
+          </span>
+          <h2 className="text-3xl font-bold">{ContentData?.h2}</h2>
 
-            <div
-              className="mt-3  text-justify"
-              dangerouslySetInnerHTML={{ __html: ContentData?.p2 }}
-            ></div>
-            <div className="gap-4 md:flex">
-              <div className="rounded-lg bg-gray-100 p-4 shadow-lg">
-                <h4 className="text-xl font-bold">
-                  Residential {ContactInfo.name} Services
-                </h4>
-                <p>
-                  Professional Residential {ContactInfo.service} in{" "}
-                  {ContentData?.name}, {State.split("-").pop()?.toUpperCase()}.
-                </p>
-              </div>
-              <div className="rounded-lg bg-gray-100 p-4 shadow-lg">
-                <h4 className="text-xl font-bold">
-                  Commercial {ContactInfo.name} Services
-                </h4>
-                <p>
-                  Commercial {ContactInfo.service} in {ContentData?.name}, {State.split("-").pop()?.toUpperCase()}.
-                </p>
-              </div>
+          <div
+            className="mt-3  text-justify"
+            dangerouslySetInnerHTML={{ __html: ContentData?.p2 }}
+          ></div>
+          <div className="gap-4 md:flex">
+            <div className="rounded-lg bg-gray-100 p-4 shadow-lg">
+              <h4 className="text-xl font-bold">
+                Residential {ContactInfo.name} Services
+              </h4>
+              <p>
+                Professional Residential {ContactInfo.service} in{" "}
+                {ContentData?.name}, {State.split("-").pop()?.toUpperCase()}.
+              </p>
+            </div>
+            <div className="rounded-lg bg-gray-100 p-4 shadow-lg">
+              <h4 className="text-xl font-bold">
+                Commercial {ContactInfo.name} Services
+              </h4>
+              <p>
+                Commercial {ContactInfo.service} in {ContentData?.name},{" "}
+                {State.split("-").pop()?.toUpperCase()}.
+              </p>
             </div>
           </div>
+        </div>
       </div>
       {/* Section 1 */}
       {/* Section 2 */}
@@ -354,11 +425,13 @@ export default function SubdomainPage({ params }: SubdomainPageProps) {
       {/* Area we Serve */}
       {slugs.length > 0 && (
         <div id="area-we-serve" className="pt-14 md:pt-28">
-        <h2 className={`  text-center text-3xl font-bold text-main`}>Cities We Serve </h2>
-        <AreaWeServe slugs={slugs} />
-      </div>
-    )}
-      
+          <h2 className={`  text-center text-3xl font-bold text-main`}>
+            Cities We Serve{" "}
+          </h2>
+          <AreaWeServe slugs={slugs} />
+        </div>
+      )}
+
       {/* Area we Serve */}
       {/* Neighborhood */}
       {/* {ContentData?.neighbourhoods ? (
